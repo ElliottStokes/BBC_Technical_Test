07b309ffe99ed68a1960e3cb7a2e984b491cfad6
ElliottStokes <35205384+ElliottStokes@users.noreply.github.com>
2019-02-25 15:13:02 +0000

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,212 @@
+// JavaScript source code
+
+
+/*var article_1 = '{"title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit", "body":[{"type": "heading","model": {"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit"}},{"type": "paragraph","model": {"text": "Etiam ornare nulla in sem pharetra, vel varius magna tempus. Praesent pulvinar eget dolor vitae elementum. Donec id velit at sem gravida tristique non vitae lorem."}},{"type": "image","model": {"url": "https://picsum.photos/640/420/?random","altText": "Vestibulum pellentesque laoreet urna, eget dignissim lorem maximus vel","height": "420","width": "640"}},{"type": "paragraph","model": {"text": "Nulla tortor nunc, posuere eu purus eget, ultricies ullamcorper sapien. Duis viverra lectus eget dolor gravida rhoncus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce molestie, odio at mattis auctor, mauris lectus luctus risus, eget pulvinar tortor erat a est."}},{"type": "paragraph","model": {"text": "Mauris accumsan maximus enim, eleifend consectetur turpis molestie malesuada. Proin vulputate dui non mattis venenatis. Nam ornare sapien eget nibh placerat, faucibus dapibus lacus consectetur."}},{"type": "list","model": {"type": "unordered","items": ["Vestibulum viverra viverra ullamcorper","Aenean ut felis hendrerit, scelerisque est sed, rhoncus sem"]}}]}'
+var article_2 = '{"title": "Praesent bibendum nec velit a fringilla. Nulla facilisi","body": [{"type": "heading","model": {"text": "Nam eu varius magna, vel molestie diam"}},{"type": "image","model": {"url": "https://picsum.photos/640/420/?random","altText": "Vestibulum pellentesque laoreet urna, eget dignissim lorem maximus vel","height": "420","width": "640"}},{"type": "paragraph","model": {"text": "Integer lobortis, magna et tempor facilisis, arcu dui finibus mauris, pellentesque fringilla massa sem non justo. Phasellus vel arcu eu eros commodo egestas at non dui. Ut ultricies quis nunc ut commodo"}},{"type": "paragraph","model": {"text": "Aenean justo urna, tempor vitae consectetur nec, ornare eget nunc. Duis ornare ipsum in quam auctor feugiat. Vestibulum sapien metus, rutrum et mattis ut, tincidunt in dolor. Praesent semper laoreet augue. Etiam lacinia hendrerit euismod."}},{"type": "image","model": {"url": "https://picsum.photos/640/420/?random","altText": "Curabitur dictum leo bibendum lacus ultrices fringilla et quis justo","height": "420","width": "640"}},{"type": "paragraph","model": {"text": "Morbi venenatis aliquam cursus. Sed eros elit, consequat id faucibus vel, interdum fringilla massa."}},{"type": "image","model": {"url": "https://picsum.photos/640/420/?random","altText": "Curabitur dictum leo bibendum lacus ultrices fringilla et quis justo","height": "420","width": "640"}},{"type": "paragraph","model": {"text": "Morbi venenatis aliquam cursus. Sed eros elit, consequat id faucibus vel, interdum fringilla massa."}}]}'
+var article_3 = '{"title": "Vestibulum posuere orci ullamcorper nisi porta, sit amet tempus nibh porta","body": [{"type": "heading","model": {"text":"Donec est neque, vulputate vitae magna non, aliquam semper lectus"}},{"type": "paragraph","model": {"text": "Nam sed tempus lectus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam sodales efficitur fermentum. Phasellus lacinia tempus odio, non mattis tellus pretium ac. Nulla facilisi. Donec tempor orci ipsum, eu euismod massa aliquet in. Fusce pretium lectus vitae dui pulvinar, vel fermentum elit porta."}},{"type": "paragraph","model":{"text": "Donec iaculis volutpat consequat. Pellentesque et lobortis tellus. In mattis, diam a tempus fringilla, augue orci tincidunt nunc, et dictum lorem eros non ante."}},{"type": "paragraph","model": {"text": "Cras dolor tellus, condimentum eu aliquet nec, facilisis a eros. Morbi sed tincidunt urna."}},{"type": "paragraph","model": {"text": "Praesent feugiat rutrum lobortis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae."}},{"type": "paragraph","model": {"text": "In imperdiet commodo nisl nec iaculis. Vivamus nunc purus, vulputate eget dapibus in, mattis sit amet tellus. Donec quis sapien a odio vestibulum egestas vel a magna."}},{"type": "paragraph","model": {"text": "Cras id condimentum dolor. Vestibulum risus leo, viverra a mi laoreet, vestibulum aliquam odio. Pellentesque aliquam placerat dolor vitae malesuada. Ut eget pretium tellus, cursus finibus ligula. Sed eros diam, porta sit amet urna eget, pretium dignissim magna. Curabitur imperdiet pharetra lectus ac venenatis."}},{"type": "paragraph","model": {"text": "Donec at tincidunt mauris, id sagittis erat. Nunc mi nisi, pellentesque et rutrum quis, congue non elit. Nullam convallis sapien quis tellus imperdiet iaculis."}},{"type": "paragraph","model": {"text": "Vivamus sollicitudin, diam non semper molestie, nisi nunc efficitur neque, id iaculis nibh ipsum sed quam. Etiam consequat tristique mi ac lacinia. Etiam leo nulla, faucibus et lobortis et, semper et quam. Cras vehicula quam non condimentum laoreet."}}]}'
+var article_4 = '{"title": "Maecenas venenatis lorem ut erat dictum, sed varius est porta","body": [{"type": "heading","model": {"text": "Maecenas venenatis lorem ut erat dictum, sed varius est porta"}},{"type": "paragraph","model": {"text": "Donec et fermentum elit, in aliquet massa. Suspendisse mollis tristique vulputate. Ut congue, quam quis volutpat egestas, nisl sem tempus urna, quis consequat arcu leo vitae turpis."}},{"type": "list","model": {"type": "unordered","items": ["Donec commodo ipsum id tellus cursus, vel fermentum elit venenatis","Nullam pulvinar orci felis, eget maximus ex imperdiet accumsan","Nulla ut lectus at est fringilla pellentesque","Duis elementum feugiat lacus non condimentum"]}},{"type": "paragraph","model": {"text": "Nunc auctor porttitor est, sed laoreet tortor mattis eget."}},{"type": "list","model": {"type": "unordered","items": ["Etiam ultrices libero eget orci maximus luctus","Sed at luctus diam, eget tincidunt arcu"]}},{"type": "paragraph","model": {"text": "Cras sodales odio et elit scelerisque, at aliquam elit facilisis. Suspendisse erat erat, egestas sed vehicula ac, finibus non velit. Curabitur fermentum iaculis velit eget semper."}}]}'
+var article_5 = '{"title": "Nulla nibh erat, pharetra at ultricies nec, tincidunt luctus arcu","body": [{"type": "heading","model": {"text": "Nulla nibh erat, pharetra at ultricies nec, tincidunt luctus arcu"}},{"type": "image","model": {"url": "https://picsum.photos/640/420/?random","altText": "Donec purus augue, lobortis et velit maximus, rutrum scelerisque ipsum","height": "420","width": "640"}},{"type": "image","model": {"url": "https://picsum.photos/640/420/?random","altText": "Etiam at nisl diam","height": "420","width": "640"}},{"type": "paragraph","model": {"text": "Suspendisse id erat a nibh rutrum consectetur. Ut porttitor arcu id nibh pellentesque, et laoreet sapien accumsan. Phasellus quis finibus ipsum. Cras malesuada eu arcu sed laoreet. Nulla egestas ac eros vel eleifend. Nam porta ornare nisl, quis gravida dolor sagittis a."}}]}'*/
+
+window.onload = function () {
+    $.ajax({
+        type: 'GET',
+        url: 'https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-1.json',
+        data: { get_param: 'value' },
+        dataType: 'json',
+        success: function (data) {
+            console.log("success");
+            console.log(data);
+            var content = '';
+            content += '<h1 id="1_title">' + data.title + '</h1>';
+            content += '<div class="draggableDiv"><h2 class="draggableName">Article 1</h2></div>';
+            content += '<h3 id="1_heading_1">' + data.body[0].model.text + '</h3>';
+            content += '<p id="1_text_2">' + data.body[1].model.text + '</p>';
+            content += '<img src="' + data.body[2].model.url + '" alt="' + data.body[2].model.altText + '" height="' + data.body[2].model.height + '" width="' + data.body[2].model.width + '" />';
+            content += '<p id="1_text_3">' + data.body[3].model.text + '</p>';
+            content += '<p id="1_text_4">' + data.body[4].model.text + '</p>';
+            content += '<ul id="1_list">';
+            content += '<li id="1_listItem_1">' + data.body[5].model.items[0] + '</li>';
+            content += '<li id="1_listItem_2">' + data.body[5].model.items[1] + '</li>';
+            content += '</ul>';
+
+            document.getElementById('article_1').innerHTML = content;
+        }
+    });
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-2.json',
+        data: { get_param: 'value' },
+        dataType: 'json',
+        success: function (data) {
+            console.log("success");
+            console.log(data);
+            var content = '';
+            content += '<h1 id="2_title">' + data.title + '</h1>';
+            content += '<div class="draggableDiv"><h2 class="draggableName">Article 2</h2></div>';
+            content += '<h3 id="2_heading_1">' + data.body[0].model.text + '</h3>'
+            content += '<img id="2_image_1" src="' + data.body[1].model.url + '" alt="' + data.body[1].model.altText + '" height="' + data.body[1].model.height + '" width="' + data.body[1].model.width + '" />';
+            content += '<p id="2_text_1">' + data.body[2].model.text + '</p>';
+            content += '<p id="2_text_2">' + data.body[3].model.text + '</p>'
+            content += '<img id="2_image_2" src="' + data.body[4].model.url + '" alt="' + data.body[4].model.altText + '" height="' + data.body[4].model.height + '" width="' + data.body[4].model.width + '" />';
+            content += '<p id="2_text_3">' + data.body[5].model.text + '</p>';
+            content += '<img id="2_image_3" src="' + data.body[6].model.url + '" alt="' + data.body[6].model.altText + '" height="' + data.body[6].model.height + '" width="' + data.body[6].model.width + '" />';
+            content += '<p id="2_text_4">' + data.body[7].model.text + '</p>';
+
+            document.getElementById('article_2').innerHTML = content;
+        }
+    });
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-3.json',
+        data: { get_param: 'value' },
+        dataType: 'json',
+        success: function (data) {
+            console.log("success");
+            console.log(data);
+            var content = '';
+            content += '<h1 id="3_title">' + data.title + '</h1>';
+            content += '<div class="draggableDiv"><h2 class="draggableName">Article 3</h2></div>';
+            content += '<h3 id="3_heading_1">' + data.body[0].model.text + '</h3>';
+            content += '<p id="3_text_1">' + data.body[1].model.text + '</p>';
+            content += '<p id="3_text_2">' + data.body[2].model.text + '</p>';
+            content += '<p id="3_text_3">' + data.body[3].model.text + '</p>';
+            content += '<p id="3_text_4">' + data.body[4].model.text + '</p>';
+            content += '<p id="3_text_5">' + data.body[5].model.text + '</p>';
+            content += '<p id="3_text_6">' + data.body[6].model.text + '</p>';
+            content += '<p id="3_text_7">' + data.body[7].model.text + '</p>';
+            content += '<p id="3_text_8">' + data.body[8].model.text + '</p>';
+
+            document.getElementById('article_3').innerHTML = content;
+        }
+    });
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-4.json',
+        data: { get_param: 'value' },
+        dataType: 'json',
+        success: function (data) {
+            console.log("success");
+            console.log(data);
+            var content = '';
+            content += '<h1 id="4_title">' + data.title + '</h1>';
+            content += '<div class="draggableDiv"><h2 class="draggableName">Article 4</h2></div>';
+            content += '<h3 id="4_heading_1">' + data.body[0].model.text + '</h3>';
+            content += '<p id="4_text_1">' + data.body[1].model.text + '</p>';
+            content += '<ul>';
+            content += '<li id="4_listItem_1">' + data.body[2].model.items[0] + '</li>';
+            content += '<li id="4_listItem_2">' + data.body[2].model.items[1] + '</li>';
+            content += '<li id="4_listItem_3">' + data.body[2].model.items[2] + '</li>';
+            content += '<li id="4_listItem_4">' + data.body[2].model.items[3] + '</li>';
+            content += '</ul>';
+            content += '<p id="4_text_2">' + data.body[3].model.text + '</p>';
+            content += '<ul>';
+            content += '<li id="4_listItem_5">' + data.body[4].model.items[0] + '</li>';
+            content += '<li id="4_listItem_6">' + data.body[4].model.items[1] + '</li>';
+            content += '</ul>';
+            content += '<p id="4_text_3">' + data.body[5].model.text + '</p>';
+
+            document.getElementById('article_4').innerHTML = content;
+        }
+    });
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-5.json',
+        data: { get_param: 'value' },
+        dataType: 'json',
+        success: function (data) {
+            console.log("success");
+            console.log(data);
+            var content = '';
+            content += '<h1 id="5_title">' + data.title + '</h1>';
+            content += '<div class="draggableDiv"><h2 class="draggableName">Article 5</h2></div>';
+            content += '<h3 id="5_heading_1">' + data.body[0].model.text + '</h3>';
+            content += '<img id="5_image_1" src="' + data.body[1].model.url + '" alt="' + data.body[1].model.altText + '" height="' + data.body[1].model.height + '" width="' + data.body[1].model.width + '" />';
+            content += '<img id="5_image_2" src="' + data.body[2].model.url + '" alt="' + data.body[2].model.altText + '" height="' + data.body[2].model.height + '" width="' + data.body[2].model.width + '" />';
+            content += '<p id="5_text_1">' + data.body[3].model.text + '</p>';
+
+            document.getElementById('article_5').innerHTML = content;
+        }
+    });
+};
+
+
+var articleIndex = 1;
+cycleArticles(articleIndex);
+
+
+function changeArticle(n) {
+    cycleArticles(articleIndex += n);
+}
+
+function cycleArticles(n) {
+    var i;
+    var articles = document.getElementsByClassName("articleDisplay");
+    var showRateArticlesDiv = document.getElementById("rateArticles");
+    var rateArticlesButton = document.getElementById("rateArticlesButton");
+
+    if (n > articles.length) { articleIndex = 1 };
+    if (n < 1) { articleIndex = articles.length };
+
+    for (i = 0; i < articles.length; i++) {
+        articles[i].style.display = "none";
+    }
+
+
+    $(articles[articleIndex - 1]).css("display", "block");
+    $(showRateArticlesDiv).css("display", "none");
+}
+
+function showArticle(n) {
+    var i;
+    var articles = document.getElementsByClassName("articleDisplay");
+
+    for (i = 0; i < articles.length; i++) {
+        articles[i].style.display = "none";
+    }
+
+    $(articles[n]).css("display", "block");
+}
+
+function showRateArticles() {
+    var showRateArticlesDiv = document.getElementById("rateArticles");
+    $(showRateArticlesDiv).css("display", "block");
+}
+
+$(function () {
+    $('.draggableDiv').draggable({
+        revert: true,
+        proxy: 'clone',
+        onStartDrag: function () {
+            $(this).draggable('proxy').css('z-index', 10);
+        }
+    });
+
+    $('.articleRateDiv').droppable({
+        onDrop: function (e, source) {
+            var name = $(source).find('h2.draggableName').text();
+            console.log(name);
+
+            $(this).html(name);
+        }
+    });
+});
+
+function submitRank() {
+    var rank_1 = document.getElementById("rank_1").innerHTML;
+    var rank_2 = document.getElementById("rank_2").innerHTML;
+    var rank_3 = document.getElementById("rank_3").innerHTML;
+    var rank_4 = document.getElementById("rank_4").innerHTML;
+    var rank_5 = document.getElementById("rank_5").innerHTML;
+
+    window.alert("Rankings:\n1)" + rank_1 + "\n2)" + rank_2 + "\n3)" + rank_3 + "\n4)" + rank_4 + "\n5)" + rank_5);
+
+    $.ajax({
+        type: "POST",
+        url: "POSTs",
+        timeout: 2000
+    })
+}
